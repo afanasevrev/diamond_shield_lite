c4476da0a02bb201299c93cd9fd94196c5d84b1e
revoly afanasev <rewoly@mail.ru>
2026-08-22 18:04:51 +0900

    Начало проекта

---
+++ backend/src/test/java/ru/server/access/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.server.access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
