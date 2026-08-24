2f24818cd5b2e4a9a8615ff60880b8ddf626de9c
revoly afanasev <rewoly@mail.ru>
2026-08-24 23:22:50 +0900

    Исправляем историю прохода

--- backend/src/main/java/ru/server/access/repository/PassageEventRepository.java
+++ backend/src/main/java/ru/server/access/repository/PassageEventRepository.java
@@ -3,9 +3,11 @@ package ru.server.access.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.server.access.entity.PassageEvent;
 
 public interface PassageEventRepository extends JpaRepository<PassageEvent, Long> {
+    @EntityGraph(attributePaths = {"person", "controller"})
     Page<PassageEvent> findAllByOrderByEventTimeDesc(Pageable pageable);
 }
